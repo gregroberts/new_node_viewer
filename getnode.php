25430609dba04746f17aa7ab2b18b51e63708ed7
gregroberts <greg.roberts1991@gmail.com>
2014-02-23 16:28:27 +0000

    Initial commit
    First stuffs.

---
+++ getnode.php
@@ -0,0 +1,16 @@
+<?php
+
+$rel = $_GET['rel'];
+$node = $_GET['node'];
+$site = $_GET['site'];
+
+$url = 'http://127.0.0.1:5000/api/v1/graph/'.$site.'/'.$node.'/'.$rel;
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $url,
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+$res = curl_exec($curl);
+echo $res;
+?>
